07f72804ae5c368c3eaa010196acf04400e29be2
Ayush <ayush.singh18@outlook.com>
2022-08-07 16:46:42 +0530

    Created Navbar Component

---
+++ src/components/NavbarComponent/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import logo from '../../images/logo.jpg'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+
+  const navListToggle = () => {
+    document.getElementById('nav-list').classList.toggle('nav-list-hidden');
+  }
+  return (
+    <>
+      <nav className='navbar'>
+        <div className="nav-logo-parent">
+          <Link to="/">
+            <img src={logo} alt="Logo (My Photo)" className='navbar-logo'/>
+          </Link>
+        </div>
+        <ul className='nav-list nav-list-hidden' id="nav-list">
+            <li className='list-item'><Link className='navbar-text' to="/">Home</Link></li>
+            <li className='list-item'><Link className='navbar-text' to="#">Portfolio</Link></li>
+            <li className='list-item'><Link className='navbar-text' to="/contact">Contact Me</Link></li>
+            <li className='list-item'><Link className='navbar-text' to="#">Certificates</Link></li>                
+        </ul>
+        <div className="menu" id="toggle-button" onClick={navListToggle}>
+          <div className="menu-line" id="menu-line-1"></div>
+          <div className="menu-line" id="menu-line-2"></div>
+          <div className="menu-line" id="menu-line-3"></div>
+        </div>
+      </nav>
+    </>
+  )
+}
