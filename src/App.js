0c641fc4ba03bda710b9e18fc8423adf9da07351
Ayush <ayush.singh18@outlook.com>
2022-08-08 19:53:05 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from './components/NavbarComponent/Navbar';
+import { ContactUs } from './components/ContactUsComponent/ContactUs';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+/**
+ * TODO:-
+ * make frontend
+ * make backend
+ * use concurrently to run backend and frontend simultaneously (video 55 cwh)
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/contact" element={<ContactUs />}/>
+      </Routes>
+    </Router>
   );
 }
 
