932dc0ba5dc846842b899ea9834fa6082d2d21fe
Ayush <ayush.singh18@outlook.com>
2022-08-24 17:27:24 +0530

    Improved navbar hover effect

--- src/components/NavbarComponent/Navbar.js
+++ src/components/NavbarComponent/Navbar.js
@@ -10,6 +10,9 @@ export const Navbar = () => {
     document.getElementById('nav-list').classList.toggle('nav-list-hidden');
   }
 
+  // Rurturn nothing if on login page to not show the navbar
+  if (location.pathname==='/signin') return;
+
   return (
     <header>
       <nav className='navbar'>
@@ -19,10 +22,10 @@ export const Navbar = () => {
           </Link>
         </div>
         <ul className='nav-list nav-list-hidden' id="nav-list">
-            <li className='list-item left'><Link className={`navbar-text ${location.pathname==='/'?'active':''}`} to="/">Home</Link></li>
-            <li className='list-item right'><Link className={`navbar-text ${location.pathname==='/about'?'active':''}`} to="/about">About Me</Link></li>
-            <li className='list-item right'><Link className={`navbar-text ${location.pathname==='/contact'?'active':''}`} to="/contact">Contact Me</Link></li>
-            <li className='list-item right'><Link className={`navbar-text ${location.pathname==='/certificate'?'active':''}`} to="/certificate">Certificates</Link></li>                
+            <li className='list-item'><Link className={`navbar-text left ${location.pathname==='/'?'active':''}`} to="/">Home</Link></li>
+            <li className='list-item'><Link className={`navbar-text right ${location.pathname==='/about'?'active':''}`} to="/about">About Me</Link></li>
+            <li className='list-item'><Link className={`navbar-text right ${location.pathname==='/contact'?'active':''}`} to="/contact">Contact Me</Link></li>
+            <li className='list-item'><Link className={`navbar-text right ${location.pathname==='/certificate'?'active':''}`} to="/certificate">Certificates</Link></li>
         </ul>
         <div className="menu" id="toggle-button" onClick={navListToggle}>
           <div className="menu-line" id="menu-line-1"></div>
