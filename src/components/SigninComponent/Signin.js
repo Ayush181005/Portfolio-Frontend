d262481b3b7c7529ffbc316d0b2ce68a5ea0ed90
Ayush <ayush.singh18@outlook.com>
2022-08-25 19:02:15 +0530

    Security changes in Signin component

--- src/components/SigninComponent/Signin.js
+++ src/components/SigninComponent/Signin.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Signin.css';
 import { useNavigate } from 'react-router-dom';
 
-export const Signin = () => {
+export const Signin = (props) => {
     const host = 'localhost';
     const port = '5000';
-
-    const [loginCredentials, setLoginCredentials] = useState({email:'', password:''});
+    const { showAlert } = props;
 
     const navigate = useNavigate(); // useNavigate hook for redirecting
 
-    const onLoginChange = (e) => setLoginCredentials({...loginCredentials, [e.target.name]: e.target.value});
+    useEffect(() => {
+        // if user is already logged in, redirect to home page
+        if (localStorage.getItem('auth-token')) {
+            showAlert('Access Denied, already logged in', 'error');
+            navigate('/');
+        }
+    }, []);
 
+    // Login Part
+    const [loginCredentials, setLoginCredentials] = useState({ email: '', password: '' });
+    const onLoginChange = (e) => setLoginCredentials({ ...loginCredentials, [e.target.name]: e.target.value });
     const handleLoginSubmit = async (e) => {
         e.preventDefault();
         const response = await fetch(`http://${host}:${port}/api/auth/login`, {
@@ -25,10 +33,48 @@ export const Signin = () => {
         const jsonResponse = await response.json();
         if (jsonResponse.success) {
             localStorage.setItem('auth-token', jsonResponse.authToken);
+            showAlert('Successfully Logged In', 'success');
             navigate('/'); // Redirect to home page
         }
         else {
-            alert("Invalid credentials");
+            let errorMsg = ""
+            jsonResponse.errors.map(error => {
+                errorMsg += error.msg + "\n";
+            });
+            showAlert(errorMsg, 'error');
+        }
+    }
+
+    // Signup Part
+    const [signupCredentials, setSignupCredentials] = useState({ name: '', email: '', password: '', passwordCheck: '' });
+    const onSignupChange = (e) => setSignupCredentials({ ...signupCredentials, [e.target.name]: e.target.value });
+    const handleSignupSubmit = async (e) => {
+        e.preventDefault();
+        const { name, email, password, passwordCheck } = signupCredentials;
+        if (password !== passwordCheck) {
+            showAlert("Passwords do not match", "error");
+            return;
+        }
+        const response = await fetch(`http://${host}:${port}/api/auth/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        if (jsonResponse.success) {
+            localStorage.setItem('auth-token', jsonResponse.authToken);
+            showAlert('Account Created Successfully and logged in!', 'success');
+            navigate('/'); // Redirect to home page
+        }
+        else {
+            let errorMsg = ""
+            jsonResponse.errors.map(error => {
+                errorMsg += error.msg + "\n";
+            });
+            showAlert(errorMsg, 'error');
         }
     }
 
@@ -37,11 +83,12 @@ export const Signin = () => {
             <div className="main">
                 <input type="checkbox" id="chk" aria-hidden="true" />
                 <div className="signup">
-                    <form action={`http://${host}:${port}/api/auth/signup`} method="post">
+                    <form onSubmit={handleSignupSubmit}>
                         <label htmlFor="chk" aria-hidden="true">Sign up</label>
-                        <input type="text" name='name' placeholder='Name' required />
-                        <input type="email" name='email' placeholder='Email' required />
-                        <input type="password" name='password' placeholder='Password' required />
+                        <input type="text" name='name' onChange={onSignupChange} placeholder='Name' minLength={3} required />
+                        <input type="email" name='email' onChange={onSignupChange} placeholder='Email' required />
+                        <input type="password" name='password' onChange={onSignupChange} placeholder='Password' minLength={5} required />
+                        <input type="password" name='passwordCheck' onChange={onSignupChange} placeholder='Confirm Password' minLength={5} required />
                         <button type='submit'>Sign up</button>
                     </form>
                 </div>
