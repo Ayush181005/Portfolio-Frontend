d7daa5a6844244a1b0d0bf19802ab4fae4b05990
Ayush <ayush.singh18@outlook.com>
2022-08-27 19:44:45 +0530

    Increased User friendliness

--- src/components/PortfolioComponent/Portfolio.js
+++ src/components/PortfolioComponent/Portfolio.js
@@ -6,14 +6,13 @@ import PortfolioContext from '../../context/portfolios/PortfolioContext'
 
 export const Portfolio = () => {
   const { portfolios, getPortfolios } = useContext(PortfolioContext);
-  
   useEffect(() => {getPortfolios()}, []); // calls only once
 
   return (
     <section className="portfolio-section">
       <div className="portfolio-container">
           <h1>Portfolio</h1>
-          {portfolios.map((portfolio, i) => {
+          {portfolios.length!=0 ? portfolios.map((portfolio, i) => {
             if (portfolio.slug) {
               // If there is a slug, then render a link to the portfolio page
               return(
@@ -28,7 +27,7 @@ export const Portfolio = () => {
                 <Card key={i} text={portfolio.title} img_url="https://www.w3schools.com/w3css/img_lights.jpg" />
               )
             }
-          })}
+          }) : <h2>No Portfolios found :(</h2>}
       </div>
     </section>
   )
