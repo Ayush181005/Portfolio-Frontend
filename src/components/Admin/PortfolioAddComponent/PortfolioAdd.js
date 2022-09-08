a88009d84634737b48feb412a14614ecfd04d5bc
Ayush <ayush.singh18@outlook.com>
2022-09-08 14:44:25 +0530

    Image Upload and Display

--- src/components/Admin/PortfolioAddComponent/PortfolioAdd.js
+++ src/components/Admin/PortfolioAddComponent/PortfolioAdd.js
@@ -1,21 +1,30 @@
-import React, { useContext, useState } from 'react';
-import PortfolioContext from '../../../context/portfolios/PortfolioContext';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 export const PortfolioAdd = (props) => {
     const { showAlert } = props;
-    const { addPortfolio } = useContext(PortfolioContext);
     const navigate = useNavigate();
 
-    // const [formData, setFormData] = useState(null);
-    // const handleOnChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const [data, setData] = useState(null);
+    const handleOnChange = (e) => setData({ ...data, [e.target.name]: e.target.value });
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const data = new FormData(e.target);
-        data.append('image', e.target.image.files[0]);
+        const formData = new FormData(); // encType='multipart/form-data'
 
-        const jsonResponse = await addPortfolio(data);
+        for (const [key, value] of Object.entries(data)) {
+            formData.append(key, value);
+        }
+        formData.append('image', e.target.image.files[0])
+
+        const response = await fetch('http://localhost:5000/api/portfolios/addportfolio', {
+            headers: {
+                'auth-token': localStorage.getItem('auth-token')
+            },
+            method: 'POST',
+            body: formData
+        });
+        const jsonResponse = await response.json();
         if (jsonResponse.success) {
             showAlert(`Added Portfolio - ${data.title}`, 'success');
             navigate('/'); // Redirect to home page
@@ -33,32 +42,31 @@ export const PortfolioAdd = (props) => {
         <section className='portfolio-add-section'>
             <h1>Add a Portfolio</h1>
             <Link to="/admin">admin</Link>&gt;<Link to="/admin/portfolios">portfolios</Link>&gt;add
-            <form action="http://localhost:5000/api/portfolios/addportfolio" method="post" encType='multipart/form-data'>
+            <form onSubmit={handleSubmit}>
                 <div className="inputBox">
                     <label htmlFor="form-title">Title</label>
-                    <input type="text" name='title' id='form-title' placeholder='Title' required />
+                    <input onChange={handleOnChange} type="text" name='title' id='form-title' placeholder='Title' required />
                 </div>
                 <div className="inputBox">
                     <label htmlFor="form-type">Type</label>
-                    <input type="text" name='type' id='form-type' placeholder='Type of the Portfolio'/>
+                    <input onChange={handleOnChange} type="text" name='type' id='form-type' placeholder='Type of the Portfolio' />
                 </div>
                 <div className="inputBox">
                     <label htmlFor="form-slug">Slug</label>
-                    <input type="text" name='slug' id='form-slug' placeholder='Slug of the Portfolio'/>
+                    <input onChange={handleOnChange} type="text" name='slug' id='form-slug' placeholder='Slug of the Portfolio' />
                 </div>
                 <div className="inputBox">
                     <label htmlFor="form-links">Links</label>
-                    <input type="text" name='links' id='form-links' placeholder='Links needed in Portfolio'/>
+                    <input onChange={handleOnChange} type="text" name='links' id='form-links' placeholder='Links needed in Portfolio' />
                     <small>Seperate by comma and space ', '</small>
                 </div>
                 <div className="inputBox">
                     <label htmlFor="form-image">Image</label>
-                    <input type="file" accept="image/*" name='image' id='form-image'/>
+                    <input onChange={handleOnChange} type="file" accept="image/*" name='image' id='form-image' />
                 </div>
-                <input type="hidden" id="authToken" name="authToken" value={localStorage.getItem('auth-token')}></input>
                 <div className="inputBox">
                     <label htmlFor="form-desc">Description</label>
-                    <textarea name="desc" id="form-desc" cols="30" rows="10" placeholder='Desc...'></textarea>
+                    <textarea onChange={handleOnChange} name="desc" id="form-desc" cols="30" rows="10" placeholder='Desc...'></textarea>
                 </div>
                 <button type="submit" className='btn submit-btn'>Save</button>
             </form>
--- src/components/PortfolioComponent/Portfolio.js
+++ src/components/PortfolioComponent/Portfolio.js
@@ -21,7 +21,7 @@ export const Portfolio = () => {
     }
   });
   // Function to filter the portfolios by type
-  const getPortfoliosFromType = (type) => portfolios.filter(portfolio => portfolio.type === type);
+  // const getPortfoliosFromType = (type) => portfolios.filter(portfolio => portfolio.type === type);
 
   return (
     <section className="portfolio-section">
@@ -31,20 +31,25 @@ export const Portfolio = () => {
           return (
             <div key={i}>
               {type ? <h2 className="text-portfolio-type">{type}</h2> : <h2 className="text-portfolio-type">Other</h2>}
-              {getPortfoliosFromType(type).map((portfolio, i) => {
-                if (portfolio.slug) {
-                  // If there is a slug, then render a link to the portfolio page
-                  return (
-                    <Link to={`/portfolio/${portfolio.slug}`} key={i}>
-                      <Card text={portfolio.title} img_url="https://www.w3schools.com/w3css/img_lights.jpg" />
-                    </Link>
-                  )
-                }
-                else {
-                  // If there is no slug, then don't render a link to the portfolio page
-                  return (
-                    <Card key={i} text={portfolio.title} img_url="https://www.w3schools.com/w3css/img_lights.jpg" />
-                  )
+              {portfolios.map((portfolio, i) => {
+                if (portfolio.type === type) {
+                  // const base64String = btoa(String.fromCharCode(...new Uint8Array(portfolio.img.data.data)));
+                  const base64String = btoa(new Uint8Array(portfolio.img.data.data).reduce(function (data, byte) {return data + String.fromCharCode(byte);}, ''));
+
+                  if (portfolio.slug) {
+                    // If there is a slug, then render a link to the portfolio page
+                    return (
+                      <Link to={`/portfolio/${portfolio.slug}`} key={i}>
+                        <Card text={portfolio.title} img_url={`data:image/png;base64,${base64String}`} />
+                      </Link>
+                    )
+                  }
+                  else {
+                    // If there is no slug, then don't render a link to the portfolio page
+                    return (
+                      <Card key={i} text={portfolio.title} img_url={`data:image/png;base64,${base64String}`} />
+                    )
+                  }
                 }
               })}
             </div>
