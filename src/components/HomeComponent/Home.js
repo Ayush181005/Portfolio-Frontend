5dc1d322ca59b32d22935fe4701861b601f97b4b
Ayush <ayush.singh18@outlook.com>
2022-09-12 20:15:41 +0530

    Added Intro part on home page

--- src/components/HomeComponent/Home.js
+++ src/components/HomeComponent/Home.js
@@ -4,8 +4,16 @@ import homeImg from '../../images/home-img.png'
 
 export const Home = () => {
   return (
-    <div className='home-container'>
-        <div className="img-container"><div className="img-container-2"><img src={homeImg} alt="Me" className='home-img'/></div></div>
-    </div>
+    <section className="intro-section">
+      <div className="intro-container">
+        <div className="content">
+          <h1>Hi, I'm <span className="name">Ayush</span></h1>
+          <small>With single 'A' 😅😁</small>
+        </div>
+        <div className="img-container">
+          <div className="img-container-2"><div className="img-container-3"><img src={homeImg} alt="Me" className='home-img' /></div></div>
+        </div>
+      </div>
+    </section>
   )
 }
