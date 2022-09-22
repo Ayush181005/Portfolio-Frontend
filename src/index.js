626f52db8d212ca422877754b98a9733427d76b3
Ayush <ayush.singh18@outlook.com>
2022-09-22 14:50:29 +0530

    temporary fix for sitemap

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { hydrate, render } from "react-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-const rootElement = document.getElementById("root");
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
