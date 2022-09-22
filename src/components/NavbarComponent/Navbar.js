5a1243e81889bf08293256b5ba14acd8b696122e
Ayush <ayush.singh18@outlook.com>
2022-09-22 14:36:29 +0530

    Fixed Bugs

--- src/components/NavbarComponent/Navbar.js
+++ src/components/NavbarComponent/Navbar.js
@@ -45,7 +45,7 @@ export const Navbar = (props) => {
             <li className='list-item'><Link className={`navbar-text right ${location.pathname==='/portfolio'?'active':''}`} to="/portfolio">Portfolio</Link></li>
             <li className='list-item'><Link className={`navbar-text right ${location.pathname==='/certificates'?'active':''}`} to="/certificates">Certificates</Link></li>
             {/* <li className='list-item'><Link className={`navbar-text right ${location.pathname==='/contact'?'active':''}`} to="/contact">Contact Me</Link></li> */}
-            <li className='list-item'><a className={`navbar-text right ${location.pathname==='/contact'?'active':''}`} href="/contact">Contact Me</a></li> {/* Temporary fix for contact page as recaptcha not loading after component change */}
+            <li className='list-item'><Link className={`navbar-text right ${location.pathname==='/contact'?'active':''}`} to="/contact">Contact Me</Link></li>
             {userData.success && <li className='list-item'>
               <button className="btn navbar-btn" onClick={dropdownClick}>Hi {userData.user.name}&nbsp;<i className="arrow down"></i></button>
               <div className="dropdown hidden">
