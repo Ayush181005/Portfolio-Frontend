153bb09eb8d490294346e2ef7570ec11d8a5fb03
Ayush <ayush.singh18@outlook.com>
2022-09-29 09:51:49 +0530

    Added react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { render } from 'react-snapshot';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+render(
+    <App />,
+    document.getElementById('root')
+)
